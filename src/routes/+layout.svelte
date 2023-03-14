b2d21cf9208b31330d5347b80f2acdfd9de995a6
96jin <gjwlsgur123@gmail.com>
2023-03-14 11:56:52 +0900

    test

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import "./style.css";
+	import * as te from "tw-elements";
+</script>
+
+<div>
+	<span on:click={() => document.body.classList.remove("dark")}>Light</span>
+	<span on:click={() => document.body.classList.add("dark")}>Dark</span>
+	<slot />
+</div>
+
+<style>
+</style>
---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,8 @@
+<script lant="ts"></script>
+
+<div class="flex">
+	<div class="bg-slate-800 h-screen p-5 pt-8 w-72">sideBar</div>
+	<div class="p-7">
+		<h1 class="text-2xl font-semibold">Home Page</h1>
+	</div>
+</div>
